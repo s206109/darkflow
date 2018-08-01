9da391350eb9817209c3ee94d57930e5e5918aae
s206109 <s206109@center.wakayama-u.ac.jp>
2018-08-01 16:13:22 +0900

    adjust for 2d

--- darkflow/net/yolov2/train.py
+++ darkflow/net/yolov2/train.py
@@ -69,6 +69,7 @@ def loss(self, net_out):
        adjusted_coords_wh = tf.sqrt(tf.exp(coords[:,:,:,2:4]) * anchors[:,:,:,0:2] / np.reshape([W, H], [1, 1, 1, 2]))
        adjusted_distance_z = tf.sqrt(tf.exp(distance[:,:,:,:1]) * anchors[:,:,:,2:] / np.reshape([W], [1, 1, 1, 1])) #適当にロスっぽ
     else:
+       adjusted_coords_xy = expit_tensor(coords[:,:,:,0:2])#シグモイド関数にかける
        adjusted_coords_wh = tf.sqrt(tf.exp(coords[:,:,:,2:4]) * np.reshape(anchors, [1, 1, B, 2]) / np.reshape([W, H], [1, 1, 1, 2]))
     coords = tf.concat([adjusted_coords_xy, adjusted_coords_wh], 3) #こいつらを繋げる
     import pdb; pdb.set_trace()
