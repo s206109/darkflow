9ad5be6a1584c5ebf5bf6035001e4ac659c2406a
s206109 <s206109@center.wakayama-u.ac.jp>
2018-08-07 02:36:56 +0900

    modified bugs

--- evaluation.py
+++ evaluation.py
@@ -72,9 +72,6 @@ bugname = []
 # dataframe for result records
 resultDF = pd.DataFrame(columns = ['iou','pc','px','py','pw','ph','pz','gc','gx','gy','gw','gh','gz'])
 for dInd in np.arange(0,len(predBoxes)): #1つ目は空なので dInd = 何ファイル目なのかの数
-    if len(predBoxes[dInd]) == 1:
-        resultDF = resultDF.append(pd.Series([0,0,0,0,0,0,0,0,0,0,0,0,0],
-                           index=resultDF.columns),ignore_index=True)
     if dInd in bugid:
         bugname.append(predBoxes[dInd][0])
         import pdb; pdb.set_trace()
