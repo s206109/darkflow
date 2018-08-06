12fe743d1b2fcc5d2f94cbc4fdbe9a316061f49f
Hirotaka Hachiya <hhachiya@sys.wakayama-u.ac.jp>
2018-08-06 15:36:28 +0900

    add plot of error of distance

--- evaluation.py
+++ evaluation.py
@@ -7,6 +7,7 @@ import json
 import re
 import pdb
 import pandas as pd
+import matplotlib.pylab as plt
 from darkflow.utils import box
 from darkflow.utils.pascal_voc_clean_xml_evaluation import pascal_voc_clean_xml
 from darkflow.utils import process
@@ -14,6 +15,7 @@ from darkflow.utils import process
 
 #-----------------------------
 # parameters
+visualPath = 'visualization'
 
 labels = ['car','Truck']
 threshold = 0.7
@@ -133,11 +135,15 @@ error50over = np.mean(np.abs((resultDF.ix[inds].gz - resultDF.ix[inds].pz).value
 std50over = np.std(np.abs((resultDF.ix[inds].gz - resultDF.ix[inds].pz).values))
 #-----------------------------
 
-
+#-----------------------------
+# plot distance prediction error
+plt.plot(['10','20','30','40','50','50 over'],[error10, error20, error30, error40, error50, error50over])
+plt.xlabel('true distance')
+plt.ylabel('absolute error')
+plt.savefig(os.path.join(visualPath,'true_distance_vs_estimation_absolute_errror.png'))
+plt.show()
+#-----------------------------
 pdb.set_trace()
-
-
-
 
 
 """
