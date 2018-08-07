9afe826765c99b355832e35412f240bff5951f1b
s206109 <s206109@center.wakayama-u.ac.jp>
2018-08-07 23:47:12 +0900

    modified for alpha

--- darkflow/cython_utils/cy_yolo2_findboxes.pyx
+++ darkflow/cython_utils/cy_yolo2_findboxes.pyx
@@ -5,6 +5,7 @@ ctypedef np.float_t DTYPE_t
 from libc.math cimport exp
 from ..utils.box import BoundBox
 from nms cimport NMS
+import math
 
 #expit
 @cython.boundscheck(False) # turn off bounds-checking for entire function
@@ -69,6 +70,7 @@ def box_constructor(meta,np.ndarray[float,ndim=3] net_out_in):
         float[:, :, :, ::1] Classes = net_out[:, :, :, 5:7]
         float[:, :, :, ::1] Bbox_pred =  net_out[:, :, :, :5]
         float[:, :, :] DISTANCE       = net_out[:, :, :, 7]
+        float[:, :, :] ALPHA         = net_out[:, :, :, 8]
         float[:, :, :, ::1] probs = np.zeros((H, W, B, C), dtype=np.float32)
 
     for row in range(H):
@@ -79,9 +81,10 @@ def box_constructor(meta,np.ndarray[float,ndim=3] net_out_in):
                 Bbox_pred[row, col, box_loop, 4] = expit_c(Bbox_pred[row, col, box_loop, 4])
                 Bbox_pred[row, col, box_loop, 0] = (col + expit_c(Bbox_pred[row, col, box_loop, 0])) / W
                 Bbox_pred[row, col, box_loop, 1] = (row + expit_c(Bbox_pred[row, col, box_loop, 1])) / H
-                Bbox_pred[row, col, box_loop, 2] = exp(Bbox_pred[row, col, box_loop, 2]) * anchors[3 * box_loop + 0] / W
-                Bbox_pred[row, col, box_loop, 3] = exp(Bbox_pred[row, col, box_loop, 3]) * anchors[3 * box_loop + 1] / H
-                DISTANCE[row, col, box_loop]     = exp(DISTANCE[row, col, box_loop]) * maxz * anchors[3 * box_loop + 2] / W
+                Bbox_pred[row, col, box_loop, 2] = exp(Bbox_pred[row, col, box_loop, 2]) * anchors[4 * box_loop + 0] / W
+                Bbox_pred[row, col, box_loop, 3] = exp(Bbox_pred[row, col, box_loop, 3]) * anchors[4 * box_loop + 1] / H
+                DISTANCE[row, col, box_loop]     = exp(DISTANCE[row, col, box_loop]) * maxz * anchors[4 * box_loop + 2] / W
+                ALPHA [row, col, box_loop]       = exp(ALPHA[row, col, box_loop]) * math.pi * anchors[4 * box_loop + 3] / W
                 #SOFTMAX BLOCK, no more pointer juggling
                 for class_loop in range(C):
                     arr_max=max_c(arr_max,Classes[row,col,box_loop,class_loop])
@@ -97,4 +100,4 @@ def box_constructor(meta,np.ndarray[float,ndim=3] net_out_in):
 
 
     #NMS
-    return NMS(np.ascontiguousarray(probs).reshape(H*W*B,C), np.ascontiguousarray(Bbox_pred).reshape(H*B*W,5), np.ascontiguousarray(DISTANCE).reshape(H*W*B))
+    return NMS(np.ascontiguousarray(probs).reshape(H*W*B,C), np.ascontiguousarray(Bbox_pred).reshape(H*B*W,5), np.ascontiguousarray(DISTANCE).reshape(H*W*B), np.ascontiguousarray(ALPHA).reshape(H*W*B))
