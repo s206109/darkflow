b107007900d210e3a68216ce83da62e2817e4a1e
s206109 <s206109@center.wakayama-u.ac.jp>
2018-08-07 15:36:25 +0900

    add rty

--- darkflow/utils/box.py
+++ darkflow/utils/box.py
@@ -10,6 +10,7 @@ class BoundBox:
         self.probs = np.zeros((classes,))
         self.filenum = int()
         self.alpha = float()
+        self.rty = float()
 
 def overlap(x1,w1,x2,w2):
     l1 = x1 - w1 / 2.;
