07f2dd7565d1aee936a8b463def0f2e607c67545
s206109 <s206109@center.wakayama-u.ac.jp>
2018-08-08 22:56:20 +0900

    modified alpha parse

--- data/kitti/original_data/training/analysis.py
+++ data/kitti/original_data/training/analysis.py
@@ -63,7 +63,8 @@ for file in files[:nData]:
 	tmp_height2d = (df[7][inds] - df[5][inds]).values
 	#tmp_alpha = np.cos(df[3][inds].values)
     #もし負の数のデータの場合、πを足して向うむきに強制的に変える
-    import pdb; pdb.set_trace()
+
+	import pdb; pdb.set_trace()
 	if   df[3][inds].values < 0:
          tmp_alpha = df[3][inds].values + math.pi
 	else:
