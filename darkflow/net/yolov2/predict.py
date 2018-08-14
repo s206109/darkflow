df7c5ef578b52faa2308abf54cf94c205aadcff9
s206109 <s206109@center.wakayama-u.ac.jp>
2018-08-15 00:30:49 +0900

    return alpha or not

--- darkflow/net/yolov2/predict.py
+++ darkflow/net/yolov2/predict.py
@@ -41,6 +41,7 @@ def postprocess(self, net_out, im, save = True):
 
 	resultsForJSON = []
 	for b in boxes:
+		print(boxes)
 		boxResults = self.process_box(b, h, w, threshold)
 		#import pdb; pdb.set_trace()
 		if boxResults is None:
