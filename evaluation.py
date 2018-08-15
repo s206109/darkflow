66704ff289b67710fef8e02aac8b538d3abf18cc
s206109 <s206109@center.wakayama-u.ac.jp>
2018-08-15 14:11:39 +0900

    change path name

--- evaluation.py
+++ evaluation.py
@@ -118,7 +118,7 @@ surveyy_g = resultDF.ix[surveyInd]['gz']
 surveyx_g = resultDF.ix[surveyInd]['ga']
 mejirushi = [-1*math.pi,(-1*math.pi)/2,math.pi/2,math.pi]
 plt.scatter(surveyx, surveyy,   c='b', s = 5, label = 'test_data')
-plt.scatter(surveyx, mejirushi,   c='b', s = 20,　marker='^', label = 'test_data')
+plt.scatter(surveyx, mejirushi,   c='g', s = 20, marker='^', label = 'test_data')
 #plt.scatter(surveyx_g, surveyy_g,   c='r', label = 'test_data')
 
 # 凡例を表示する
