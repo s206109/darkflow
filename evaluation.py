f9daaa7a4064528d2514ff11395cb7e162b4f063
s206109 <s206109@center.wakayama-u.ac.jp>
2018-08-15 11:32:59 +0900

    change path name

--- evaluation.py
+++ evaluation.py
@@ -113,11 +113,11 @@ for dInd in np.arange(0,len(predBoxes)): #dInd = 何ファイル目なのかの�
 #-----------------------------
 surveyInd = np.where(resultDF['iou'] > 0.7)[0] #iou0.7のものを用意
 surveyx = resultDF.ix[surveyInd]['ga']
-surveyy = resultDF.ix[surveyInd]['pz']
+surveyy = resultDF.ix[surveyInd]['pz-gz']
 surveyy_g = resultDF.ix[surveyInd]['gz']
 surveyx_g = resultDF.ix[surveyInd]['ga']
 plt.scatter(surveyx, surveyy,   c='b', label = 'test_data')
-plt.scatter(surveyx_g, surveyy_g,   c='r', label = 'test_data')
+#plt.scatter(surveyx_g, surveyy_g,   c='r', label = 'test_data')
 
 # 凡例を表示する
 plt.legend()
