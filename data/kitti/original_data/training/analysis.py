e1d1bf31a8f4488c953907f4fbeedf0020a6d620
s206109 <s206109@center.wakayama-u.ac.jp>
2018-08-16 03:20:29 +0900

    test

--- data/kitti/original_data/training/analysis.py
+++ data/kitti/original_data/training/analysis.py
@@ -78,7 +78,7 @@ for file in files[:nData]:
 	tmp_alpha = df[3][inds].values
 	tmp_vecX = np.cos(df[3][inds].values)
 	tmp_vecY = np.sin(df[3][inds].values)
-	for vind in len(tmp_alpha):
+	for vind in range(len(tmp_alpha)):
           tmp_vecX[vind] = (tmp_vecX[vind] + 13)/2
           tmp_vecY[vind] = (tmp_vecY[vind] + 13)/2
 
