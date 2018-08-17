a765461562ca1525572a0c028855d1e21a3e87cb
s206109 <s206109@center.wakayama-u.ac.jp>
2018-08-18 02:16:19 +0900

    change the displae

--- evaluation.py
+++ evaluation.py
@@ -40,7 +40,7 @@ gtBoxes.sort()
 # load predicted boxes as predBoxes
 # jsonの読み込み
 cur_dir = os.getcwd()
-os.chdir('data/kitti/set1/PNGImagesTest/out-20000')
+os.chdir('data/kitti/set1/PNGImagesTest/out-vector-30000')
 jsonFiles = glob.glob('*.json')
 
 predBoxes = [0 for re2 in range(len(jsonFiles))]
