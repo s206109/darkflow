228361ff9594cc0f815e8f8140d5bb9184fc6331
s206109 <s206109@center.wakayama-u.ac.jp>
2018-08-20 01:28:40 +0900

    miss

--- evaluation.py
+++ evaluation.py
@@ -58,8 +58,8 @@ for i, file in enumerate(jsonFiles):
            cdBox[j][3] = js[j]["bottomright"]["x"]
            cdBox[j][4] = js[j]["bottomright"]["y"]
            cdBox[j][5] = js[j]["dist"]
-           cdBox[j][6] = js[j]["vecX"]
-           cdBox[j][7] = js[j]["vecY"]
+           cdBox[j][7] = js[j]["vecX"]
+           cdBox[j][8] = js[j]["vecY"]
        cdBox.insert(0,int(re.sub(r'\D', '',file))) # ファイル名からどのファイルかインデックスとして抽出
     predBoxes[i] = cdBox
 
