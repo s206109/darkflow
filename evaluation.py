039e0c051e22aae87bc108952ebc60df1291aead
s206109 <s206109@center.wakayama-u.ac.jp>
2018-09-05 01:26:43 +0900

    change out name

--- evaluation.py
+++ evaluation.py
@@ -55,7 +55,7 @@ gtBoxes.sort()
 # load predicted boxes as predBoxes
 # jsonの読み込み
 cur_dir = os.getcwd()
-os.chdir('data/kitti/set1/PNGImagesTest/out-30000-vector-ex')
+os.chdir('data/kitti/set1/PNGImagesTest/out-30000-vector-ex-2')
 jsonFiles = glob.glob('*.json')
 
 predBoxes = [0 for re2 in range(len(jsonFiles))]
