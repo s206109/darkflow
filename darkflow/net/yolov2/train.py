f3dbca2154544dc4e24657c4930f1cd15bbd289f
s206109 <s206109@center.wakayama-u.ac.jp>
2018-09-14 21:54:49 +0900

    try

--- darkflow/net/yolov2/train.py
+++ darkflow/net/yolov2/train.py
@@ -171,7 +171,7 @@ def loss(self, net_out):
          #true = tf.concat([true, _alpha], 3)
          #wght = tf.concat([wght, alpid], 3)
          #true = tf.concat([true, _vecX, _vecY], 3)
-         wght = tf.concat([wght, veXid, veYid], 3)
+         wght = tf.concat([wght, veXid], 3)
 
     import pdb; pdb.set_trace()
     print('Building {} loss'.format(m['model']))
