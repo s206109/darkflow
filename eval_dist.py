0c7bf71d6660daaabf14ffa6df4c8bad67bbfdf9
s206109 <s206109@center.wakayama-u.ac.jp>
2018-09-24 01:39:24 +0900

    confirm error

--- eval_dist.py
+++ eval_dist.py
@@ -189,18 +189,18 @@ surveyy_g = resultDF.ix[surveyInd]['gz']
 surveyx_g = resultDF.ix[surveyInd]['ga']
 mejirushiy = [0, 0, 0, 0, 0]
 mejirushi = [-1*math.pi,(-1*math.pi)/2, 0 ,math.pi/2,math.pi]
-plt.scatter(surveyx, surveyy,   c='b', s = 5, label = 'test　data')
+plt.scatter(surveyx, surveyy,   c='b', s = 5, label = 'test data')
 for ssk in mejirushi:
      plt.vlines([ssk], -15, 15, "red", linestyles='dashed')
 #plt.scatter(surveyx_g, surveyy_g,   c='r', label = 'test_data')
 
 # 凡例を表示する
-plt.legend(prop = {'size':12,})
-plt.xlabel('object angle')
-plt.ylabel('distance error')
+plt.legend(prop = {'size':18,})
+plt.xlabel('object angle',fontsize = 18)
+plt.ylabel('distance error',fontsize = 18)
 
 # グラフのタイトルを設定する
-plt.title("Distribution of distance error")
+plt.title("Distribution of distance error",fontsize = 18)
 
 # 表示する
 plt.show()
