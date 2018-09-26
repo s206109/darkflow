95e28efdc9f7947b4a65d8f11aa12333ae37d909
s206109 <s206109@center.wakayama-u.ac.jp>
2018-09-26 19:05:28 +0900

    compute error

--- eval_dist.py
+++ eval_dist.py
@@ -265,7 +265,7 @@ plt.plot(['10','20','30','40','40 over'],[error10, error20, error30, error40, er
 #plt.plot(['10','20','30','40','40 over'],[1.3878909524222403, 1.7428688349630319, 2.771728648535813, 3.5718634061115546, 3.5744018749480553])
 
 #plt.plot(['10','20','30','40','40 over'],[error10_a, error20_a, error30_a, error40_a, error40over_a])
-plt.legend()
+plt.legend(fontsize = 18)
 plt.xlabel('true distance',fontsize = 18)
 plt.ylabel('absolute error',fontsize = 18)
 plt.savefig(os.path.join(visualPath,'true_distance_vs_estimation_absolute_errror.png'))
