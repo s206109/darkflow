79d48427ede080fb11f5d924411e12b9843976ae
s206109 <s206109@center.wakayama-u.ac.jp>
2018-09-26 18:26:26 +0900

    compute error

--- eval_dist.py
+++ eval_dist.py
@@ -260,13 +260,14 @@ std40over_a = np.std(np.abs((resultDF.ix[inds].gz - resultDF.ix[inds].pz).values
 """
 #-----------------------------
 # plot distance prediction error
-plt.plot(['10','20','30','40','40 over'],[error10, error20, error30, error40, error40over])
+plt.plot(['10','20','30','40','40 over'],[error10, error20, error30, error40, error40over],label = 'estimation with 2.5D anchor')
 #plt.plot(['10','20','30','40','40 over'],[1.5,1,1.85,2.3,3])
 #plt.plot(['10','20','30','40','40 over'],[1.3878909524222403, 1.7428688349630319, 2.771728648535813, 3.5718634061115546, 3.5744018749480553])
 
 #plt.plot(['10','20','30','40','40 over'],[error10_a, error20_a, error30_a, error40_a, error40over_a])
-plt.xlabel('true distance')
-plt.ylabel('absolute error')
+plt.legend()
+plt.xlabel('true distance',fontsize = 18)
+plt.ylabel('absolute error',fontsize = 18)
 plt.savefig(os.path.join(visualPath,'true_distance_vs_estimation_absolute_errror.png'))
 plt.show()
 #-----------------------------
