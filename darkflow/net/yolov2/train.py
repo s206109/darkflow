9304a8a2a0b3d2c40caa88217854671dfa780ebe
s206109 <s206109@center.wakayama-u.ac.jp>
2018-09-27 04:14:33 +0900

    try

--- darkflow/net/yolov2/train.py
+++ darkflow/net/yolov2/train.py
@@ -187,7 +187,7 @@ def loss(self, net_out):
     print('Building {} loss'.format(m['model']))
     loss = tf.pow(adjusted_net_out - true, 2)
     #loss = tf.concat([loss,difal], 3)
-    #loss = tf.multiply(loss, wght)
+    loss = tf.multiply(loss, wght)
     if self.FLAGS.alpha:
         loss = tf.reshape(loss, [-1, H*W*B*(4 + 1 + 1 +1 + C)])
     else:
