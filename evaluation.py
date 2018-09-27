02f9bb96fb681c998b0607362dea0705622efb4b
s206109 <s206109@center.wakayama-u.ac.jp>
2018-09-27 14:43:30 +0900

    chg

--- evaluation.py
+++ evaluation.py
@@ -287,7 +287,7 @@ std40over = np.std(np.abs((resultDF.ix[inds].gz - resultDF.ix[inds].pz).values))
 #-----------------------------
 # plot distance prediction error
 #plt.plot(['[0-10]','[10-20]','[20-30]','[30-40]','[40 over]'],[error10, error20, error30, error40, error40over],label = 'estimation with 2.5D + orientation anchor')
-plt.plot(['[-3pi/4<]','[-3pi/4:-pi/4]','[-pi/4:pi/4]','[pi/4:3pi/4]','[<3pi/4]'],[error1, error2, error3, error4, error5],label = 'estimation with 2.5D + orientation anchor')
+plt.plot([error1, error2, error3, error4, error5],label = 'estimation with 2.5D + orientation anchor')
 #plt.plot(['10','20','30','40','40 over'],[1.5,1,1.85,2.3,3])
 #plt.plot(['10','20','30','40','40 over'],[1.3878909524222403, 1.7428688349630319, 2.771728648535813, 3.5718634061115546, 3.5744018749480553])
 
