ecb12e973c61485ef4478ccdbb59dcaad9222950
s206109 <s206109@center.wakayama-u.ac.jp>
2018-09-28 00:00:45 +0900

    change

--- eval_dist.py
+++ eval_dist.py
@@ -240,7 +240,9 @@ plt.plot(['[0-10]','[10-20]','[20-30]','[30-40]','[40 over]'],[error10, error20,
 #print([error10, error20, error30, error40, error40over])
 #plt.plot(['10','20','30','40','40 over'],[error10_a, error20_a, error30_a, error40_a, error40over_a])
 plt.legend(fontsize = 12)
-plt.errorbar(['[0-10]','[10-20]','[20-30]','[30-40]','[40 over]'], [error10, error20, error30, error40, error40over], yerr=[std10 /10,std20/10 ,std30/10, std40/10, std40over/10], xerr=None,fmt='ro', ecolor='')
+std_error = [std10 /10,std20/10 ,std30/10, std40/10, std40over/10]
+print(std_error)
+plt.errorbar(['[0-10]','[10-20]','[20-30]','[30-40]','[40 over]'], [error10, error20, error30, error40, error40over], yerr=std_error, xerr=None,fmt='ro', ecolor='')
 plt.xlabel('true distance',fontsize = 18)
 plt.ylabel('absolute error',fontsize = 18)
 plt.savefig(os.path.join(visualPath,'true_distance_vs_estimation_absolute_errror.png'))
