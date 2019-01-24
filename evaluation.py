cb386e0ef11b291ac05923922bf7351ce90b3b84
s206109 <s206109@center.wakayama-u.ac.jp>
2019-01-25 03:08:47 +0900

    test

--- evaluation.py
+++ evaluation.py
@@ -273,7 +273,7 @@ std40over_a = np.std(np.abs((resultDF.ix[inds].gz - resultDF.ix[inds].pz).values
 """
 #-----------------------------
 # plot distance prediction error
-plt.plot(['[0-10]','[10-20]','[20-30]','[30-40]','[40 over]'],[error10, error20, error30, error40, error40over],label = 'estimation with 2.5D + orientation anchor',color = 'blue')
+plt.plot(['[0-10]','[10-20]','[20-30]','[30-40]','[40 over]'],[error10, error20, error30, error40, error40over],label = 'estimation with 2.5D + orientation anchor (10)',color = 'blue')
 #plt.plot(['[0-10]','[10-20]','[20-30]','[30-40]','[40 over]'],[error10_a, error20_a, error30_a, error40_a, error40over_a],label = 'orientation with 2.5D + orientation anchor')
 #plt.plot(['1:[-3pi/4<]','2:[-3pi/4:-pi/4]','3:[-pi/4:pi/4]','4:pi/4:3pi/4]','5:[<3pi/4]'],[error1, error2, error3, error4, error5],label = 'estimation with 2.5D + orientation anchor')
 #plt.plot(['1:[-3pi/4<]','2:[-3pi/4:-pi/4]','3:[-pi/4:pi/4]','4:pi/4:3pi/4]','5:[<3pi/4]'],[bunsan1, bunsan2, bunsan3, bunsan4, bunsan5],label = 'bunsan with 2.5D + orientation anchor')
@@ -282,7 +282,8 @@ plt.plot(['[0-10]','[10-20]','[20-30]','[30-40]','[40 over]'],[error10, error20,
 
 
 plt.plot(['[0-10]','[10-20]','[20-30]','[30-40]','[40 over]'],[1.1577788484456362, 1.6437140840763669, 3.6163843362981618, 5.0210139905144189, 3.9699841791788741],label = 'estimation with 2.5D anchor (10)',color = 'red')
-plt.plot(['[0-10]','[10-20]','[20-30]','[30-40]','[40 over]'],[1.2321160611658288,1.5971189845510934,2.6035604366255396, 3.4388417674045928,4.4292243364122186],label = 'estimation with 2.5D anchor(20)',color = 'orange')#20の場合
+plt.plot(['[0-10]','[10-20]','[20-30]','[30-40]','[40 over]'],[1.2321160611658288,1.5971189845510934,2.6035604366255396, 3.4388417674045928,4.4292243364122186],label = 'estimation with 2.5D anchor (20)',color = 'orange')#20の場合
+plt.plot(['[0-10]','[10-20]','[20-30]','[30-40]','[40 over]'],[1.2490914781316316,1.4512085404974877,2.6010162929318983,3.4977290793026192,3.748560212787829],label = 'estimation with 2.5D + orientation anchor (20)',color = 'green')#20の場合
 
 #plt.xlabel('true distance',fontsize = 18)
 std_error = [std10/10,std20/10 ,std30/10, std40/10, std40over/10]
@@ -292,7 +293,9 @@ std_error2 = [0.095810134099037647,0.12695240038297473,0.21091872336211481,0.242
 
 plt.errorbar(['[0-10]','[10-20]','[20-30]','[30-40]','[40 over]'], [error10, error20, error30, error40, error40over], std_error,fmt = 'o',color = 'blue')
 plt.errorbar(['[0-10]','[10-20]','[20-30]','[30-40]','[40 over]'], [1.1577788484456362, 1.6437140840763669, 3.6163843362981618, 5.0210139905144189, 3.9699841791788741], std_error2,fmt = 'o',color = 'red')
-plt.errorbar(['[0-10]','[10-20]','[20-30]','[30-40]','[40 over]'], [1.2321160611658288,1.5971189845510934,2.6035604366255396, 3.4388417674045928,4.4292243364122186], std_error2,fmt = 'o',color = 'red')#20の場合
+plt.errorbar(['[0-10]','[10-20]','[20-30]','[30-40]','[40 over]'], [1.2321160611658288,1.5971189845510934,2.6035604366255396, 3.4388417674045928,4.4292243364122186], std_error2,fmt = 'o',color = 'orange')#20の場合
+plt.errorbar(['[0-10]','[10-20]','[20-30]','[30-40]','[40 over]'], [0.95114993464107489,1.1706906856230692,1.9911843180444384,2.5516569571467587,4.2120348287731071], std_error2,fmt = 'o',color = 'green')#20の場合
+
 
 plt.legend(fontsize = 10)
 plt.xlabel('true distance[m]',fontsize = 16)
