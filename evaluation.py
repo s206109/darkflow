319c8d64096b984e0d472edb5bf4dbf9c093f0b3
s206109 <s206109@center.wakayama-u.ac.jp>
2019-02-12 03:36:37 +0900

    test

--- evaluation.py
+++ evaluation.py
@@ -180,7 +180,7 @@ plt.title("test_datas")
 plt.show()
 #-----------------------------
 
-"""
+
 import pdb; pdb.set_trace()
 
 #-----------------------------
@@ -240,7 +240,7 @@ std40over = np.std(np.abs((resultDF.ix[inds].gz - resultDF.ix[inds].pz).values))
 
 
 pdb.set_trace()
-
+"""
 
 #-----------------------------
 
