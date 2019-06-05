66f69e08252393a7e54a447b61d79a6aa82c01df
s206109 <s206109@center.wakayama-u.ac.jp>
2019-06-05 09:55:24 +0900

    modifiy cython

--- darkflow/cython_utils/cy_yolo2_findboxes.pyx
+++ darkflow/cython_utils/cy_yolo2_findboxes.pyx
@@ -69,10 +69,10 @@ def box_constructor(meta,np.ndarray[float,ndim=3] net_out_in):
     ###########################
     cdef:
         float[:, :, :, ::1] net_out = net_out_in.reshape([H, W, B, net_out_in.shape[2]/B])
-        float[:, :, :, ::1] Classes = net_out[:, :, :, 5:7]
+        float[:, :, :, ::1] Classes = net_out[:, :, :, 5:6]
         float[:, :, :, ::1] Bbox_pred =  net_out[:, :, :, :5]
         float[:, :, :] DISTANCE       = net_out[:, :, :, 7]
-        float[:, :, :] ALPHA         = net_out[:, :, :, 8]
+        #float[:, :, :] ALPHA         = net_out[:, :, :, 8]
         #float[:, :, :] VECX         = net_out[:, :, :, 8]
         #float[:, :, :] VECY         = net_out[:, :, :, 9]
         float[:, :, :, ::1] probs = np.zeros((H, W, B, C), dtype=np.float32)
@@ -89,7 +89,7 @@ def box_constructor(meta,np.ndarray[float,ndim=3] net_out_in):
                 Bbox_pred[row, col, box_loop, 3] = exp(Bbox_pred[row, col, box_loop, 3]) * anchors[ANC * box_loop + 1] / H
                 DISTANCE[row, col, box_loop]     = exp(DISTANCE[row, col, box_loop]) * maxz * anchors[ANC * box_loop + 2] / W
                 #ALPHA [row, col, box_loop]       = exp(ALPHA[row, col, box_loop]) * anchors[ANC * box_loop + 3] / W
-                ALPHA [row, col, box_loop]       = ALPHA[row, col, box_loop]
+                #ALPHA [row, col, box_loop]       = ALPHA[row, col, box_loop]
                 #VECX [row, col, box_loop]       = exp(VECX[row, col, box_loop]) * anchors[ANC * box_loop + 3] / W
                 #VECY [row, col, box_loop]       = exp(VECY[row, col, box_loop]) * anchors[ANC * box_loop + 4] / W
                 #SOFTMAX BLOCK, no more pointer juggling
@@ -107,6 +107,6 @@ def box_constructor(meta,np.ndarray[float,ndim=3] net_out_in):
 
 
     #NMS
-    return NMS(np.ascontiguousarray(probs).reshape(H*W*B,C), np.ascontiguousarray(Bbox_pred).reshape(H*B*W,5), np.ascontiguousarray(DISTANCE).reshape(H*W*B), np.ascontiguousarray(ALPHA).reshape(H*W*B))
-    #return NMS(np.ascontiguousarray(probs).reshape(H*W*B,C), np.ascontiguousarray(Bbox_pred).reshape(H*B*W,5), np.ascontiguousarray(DISTANCE).reshape(H*W*B))
+    #return NMS(np.ascontiguousarray(probs).reshape(H*W*B,C), np.ascontiguousarray(Bbox_pred).reshape(H*B*W,5), np.ascontiguousarray(DISTANCE).reshape(H*W*B), np.ascontiguousarray(ALPHA).reshape(H*W*B))
+    return NMS(np.ascontiguousarray(probs).reshape(H*W*B,C), np.ascontiguousarray(Bbox_pred).reshape(H*B*W,5), np.ascontiguousarray(DISTANCE).reshape(H*W*B))
     #return NMS(np.ascontiguousarray(probs).reshape(H*W*B,C), np.ascontiguousarray(Bbox_pred).reshape(H*B*W,5), np.ascontiguousarray(DISTANCE).reshape(H*W*B) , np.ascontiguousarray(VECX).reshape(H*W*B) , np.ascontiguousarray(VECY).reshape(H*W*B))
