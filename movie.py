83dd730fed82e548044f3cb0618d13ceae631457
s206109 <s206109@center.wakayama-u.ac.jp>
2019-06-18 23:06:42 +0900

    e

--- movie.py
+++ movie.py
@@ -3,10 +3,10 @@ import cv2
 fourcc = cv2.VideoWriter_fourcc('m','p','4','v')
 video = cv2.VideoWriter('video.mp4', fourcc, 20.0, (1242, 375))
 image_path = "training/image_2/"
-for i in range(1, 20):
-    img = cv2.imread(image_path + '000000_{0:02d}.png'.format(i))
-    img = cv2.resize(img, (1242,375))
-    import pdb; pdb.set_trace()
-    video.write(img)
+for j in range(1, 199):
+   for i in range(1, 20):
+       img = cv2.imread(image_path + '{0:06d}_{0:02d}.png'.format(j,i))
+       img = cv2.resize(img, (1242,375))
+       video.write(img)
 
 video.release()
