c717bf1c25d7754e640054ed86e7f2c2fff97b7b
s206109 <s206109@center.wakayama-u.ac.jp>
2019-06-26 23:17:00 +0900

    change font size

--- oc_demo.py
+++ oc_demo.py
@@ -67,7 +67,7 @@ while True:
             # 検出位置の表示
             cv2.rectangle(frame, (tlx, tly), (brx, bry), colors[class_num], 2)
             text = label + " " + ('%.2f' % dist)
-            cv2.putText(frame, text, (tlx+10, tly-5), cv2.FONT_HERSHEY_SIMPLEX, 0.8, colors[class_num], 2)
+            cv2.putText(frame, text, (tlx+10, tly-5), cv2.FONT_HERSHEY_SIMPLEX, 0.08 * dist, colors[class_num], 2)
 
 
     # 保存
