8c41d2ec6d527a4421eb4a16d1d9776c371e502b
s206109 <s206109@center.wakayama-u.ac.jp>
2019-08-07 11:09:56 +0900

    a

--- ext.py
+++ ext.py
@@ -12,9 +12,13 @@ saver = tf.train.import_meta_graph('./model_ckpt/linear.meta')
 saver.restore(sess,tf.train.latest_checkpoint('./model_ckpt'))
 
 graph = tf.get_default_graph()
-weight = graph.get_tensor_by_name("weight1:0")
-bias = graph.get_tensor_by_name("bias1:0")
+_W1 = graph.get_tensor_by_name("weight1:0")
+_B1 = graph.get_tensor_by_name("bias1:0")
+_W2 = graph.get_tensor_by_name("weight2:0")
+_B2 = graph.get_tensor_by_name("bias2:0")
 
 # WとBをプリント
 print(sess.run('weight1:0'))
 print(sess.run('bias1:0'))
+print(sess.run('weight2:0'))
+print(sess.run('bias2:0'))
