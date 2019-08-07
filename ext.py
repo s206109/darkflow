2ca77a67151bba60e1727bdf4416ce54bb60a61f
s206109 <s206109@center.wakayama-u.ac.jp>
2019-08-07 11:06:41 +0900

    a

--- ext.py
+++ ext.py
@@ -10,3 +10,7 @@ saver = tf.train.import_meta_graph('./model_ckpt/linear.meta')
 
 # モデルの復元
 saver.restore(sess,tf.train.latest_checkpoint('./model_ckpt'))
+
+graph = tf.get_default_graph()
+weight = graph.get_tensor_by_name("weight:0")
+bias = graph.get_tensor_by_name("bias:0")
