cf17e935f96e62b123f8aebabfe3ceaf23d2fcfd
s206109 <s206109@center.wakayama-u.ac.jp>
2019-08-07 11:07:55 +0900

    a

--- ext.py
+++ ext.py
@@ -14,3 +14,7 @@ saver.restore(sess,tf.train.latest_checkpoint('./model_ckpt'))
 graph = tf.get_default_graph()
 weight = graph.get_tensor_by_name("weight1:0")
 bias = graph.get_tensor_by_name("bias1:0")
+
+# WとBをプリント
+print(sess.run('weight1:0'))
+print(sess.run('bias1:0'))
