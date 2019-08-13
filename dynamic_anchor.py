d4aca166b347c52b4fca7ca11bc0ba175312c2af
s206109 <s206109@center.wakayama-u.ac.jp>
2019-08-13 12:02:07 +0900

    a

--- dynamic_anchor.py
+++ dynamic_anchor.py
@@ -40,6 +40,7 @@ _B1 = graph.get_tensor_by_name("bias1:0")
 _W2 = graph.get_tensor_by_name("weight2:0")
 _B2 = graph.get_tensor_by_name("bias2:0")
 print("loading dynamic anchor .....................")
+sess.close()
 def re_dynamic(x):
     # Define your equation Ypred = X * W + b
     hidden = tf.add(_B1,tf.matmul(x,_W1))
