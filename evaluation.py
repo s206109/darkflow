f981468af09cc8aacb2e6059afa1863280ba3934
s206109 <s206109@center.wakayama-u.ac.jp>
2019-10-29 18:08:16 +0900

    a

--- evaluation.py
+++ evaluation.py
@@ -28,7 +28,7 @@ threshold = 0.7
 # load config values
 #_, meta = process.parser('cfg/tiny-yolo-kitti-3d.cfg')
 #_, meta = process.parser('cfg/tiny-yolo-kitti-3d_13x13.cfg')
-_, meta = process.parser('cfg/tiny-yolo-kitti-3d-10.cfg')
+_, meta = process.parser('cfg/tiny-yolo-kitti-3d-10-dynamic.cfg')
 #-----------------------------
 
 #-----------------------------
